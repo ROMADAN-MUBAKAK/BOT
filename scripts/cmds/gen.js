cd54b62bbe68c98f08ab892ad4f4a96fff59c433
Team <sigmaaryan@yahoo.com>
2025-03-14 16:46:57 +0600

    Update gen.js

--- scripts/cmds/gen.js
+++ scripts/cmds/gen.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const axios = require('axios');
 const fs = require("fs-extra");
 const path = require('path');
@@ -53,3 +54,5 @@ async function getApiUrl() {
         return null;
     }
 }
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
