24b2a25609c278f1deb342d98b56fe631c9926d2
Team <sigmaaryan@yahoo.com>
2025-03-14 16:50:50 +0600

    Update xl.js

--- scripts/cmds/xl.js
+++ scripts/cmds/xl.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require("fca-liane-utils");
 const axios = require('axios');
 
 module.exports = {
@@ -71,3 +72,5 @@ module.exports = {
  }
  }
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
